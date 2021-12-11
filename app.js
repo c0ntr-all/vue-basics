bf739a653c5043523fae6c6250dfaef4f7cc13de
unrealserg <serg.taturin@gmail.com>
2021-12-12 00:52:36 +0300

    02-09: Добавляем реактивности

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const App = {
-    data() {
-        return {
-            placeholderString: 'Введите название заметки',
-            title: 'Список заметок',
-            inputValue: '',
-            notes: ['Заметка 1', 'Заметка 2']
-        }
-    },
-    methods: {
-        inputChangehandler(event) {
-            this.inputValue = event.target.value
-        }
+  data() {
+    return {
+      placeholderString: 'Введите название заметки',
+      title: 'Список заметок',
+      inputValue: '',
+      notes: ['Заметка 1', 'Заметка 2']
     }
+  },
+  methods: {
+    inputChangeHandler(event) {
+      this.inputValue = event.target.value
+    },
+    addNewNote() {
+      this.notes.push(this.inputValue);
+      this.inputValue = '';
+    }
+  }
 }
 
 Vue.createApp(App).mount('#app')
