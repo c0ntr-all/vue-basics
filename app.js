2902f91ff4d9d31a8306f709c4d795bafdf8b61c
unrealserg <serg.taturin@gmail.com>
2021-12-11 20:52:09 +0300

    02-07: Пример. Создаём двустороннее связывание

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const App = {
     data() {
         return {
             placeholderString: 'Введите название заметки',
-            title: 'Список заметок'
+            title: 'Список заметок',
+            inputValue: ''
+        }
+    },
+    methods: {
+        inputChangehandler(event) {
+            this.inputValue = event.target.value
         }
     }
 }
