b97f419b8649b6ba99750339d2af045affd781c2
unrealserg <serg.taturin@gmail.com>
2021-12-12 17:33:31 +0300

    03-08: Про разные модификаторы

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ Vue.createApp({
     },
     items: [1, 2, 3, 4, 5, 6]
   }),
+  methods: {
+    addItem(event) {
+      this.items.unshift(this.$refs.myInput.value)
+      this.$refs.myInput.value = ''
+      console.log(event.key)
+    }
+  },
   computed: {
     evenItems() {
       return this.items.filter(i => i % 2 === 0)
