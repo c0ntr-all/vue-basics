a03c8bc2a0c4efe3636773b36c3b74ad5c05fe77
unrealserg <serg.taturin@gmail.com>
2021-12-12 17:03:25 +0300

    03-04: Циклы с v-for

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 Vue.createApp({
   data: () => ({
     myHtml: '<h1>Vue 3 App</h1>',
-    title: 'Я есть Грут'
+    title: 'Я есть Грут',
+    person: {
+      firstName: 'Vladilen',
+      lastName: 'Minin',
+      age: 27
+    },
+    items: [1, 2, 3, 4, 5, 6]
   })
 }).mount('#app')
