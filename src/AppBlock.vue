e97b7f55abf7e16c698753dac4a7ef7d49f36237
unrealserg <serg.taturin@gmail.com>
2021-12-20 02:30:11 +0300

    06-13: Как работают slots

---
+++ src/AppBlock.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card">
+    <slot name="header"></slot>
+    <!-- Дефолтное значение, если ничего не передается из основного компонента. name указывать необязательно -->
+    <slot name="default">No text</slot>
+    <div v-id="$slots.footer">
+      <slot name="footer"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    mounted() {
+      console.log(this.$slots)
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
