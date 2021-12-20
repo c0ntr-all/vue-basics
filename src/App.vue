3f07a93dcd6c8f437f29c75828163b5bca2c9656
unrealserg <serg.taturin@gmail.com>
2021-12-21 01:36:09 +0300

    06-17: Расширенный синтаксис computed свойств

--- src/App.vue
+++ src/App.vue
@@ -29,13 +29,22 @@ export default {
       active: 'one' //two
     }
   },
+  mounted() {
+    setTimeout(() => {
+      this.componentName = 'new comp name'
+    }, 1500)
+  },
   computed: {
-    componentName() {
-      // if(this.active === 'one') {
-      //   return 'app-text-one'
-      // }
-      // return 'app-text-two'
-      return 'app-text-' + this.active
+    // componentName() {
+    //   return 'app-text-' + this.active
+    // },
+    componentName: {
+      get() {
+        return 'app-text-' + this.active
+      },
+      set(value) {
+        console.log('componentName', value)
+      }
     },
     oneColor() {
       return this.active === 'one' ? 'primary' : ''
