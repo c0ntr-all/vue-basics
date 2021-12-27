e354389069f0bbeb240f030dedf51b5479c1a343
c0ntr-all <serg.taturin@gmail.com>
2021-12-28 00:24:51 +0300

    09-08: Пример. Создание плагина мультиязычности

--- 09-vue-reusability/src/App.vue
+++ 09-vue-reusability/src/App.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="container">
     <div class="card">
-      <h2>Как работают плагины во Vue?</h2>
+      <h2>{{ $i18n('app.title') }}</h2>
 
-      <button class="btn" @click="$alert('Wow It is working!')">Alert</button>
+      <button class="btn" @click="changeLang">{{ $i18n('app.changeBtn')}}</button>
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  inject: ['changeI18N'],
+  methods: {
+    changeLang() {
+      this.changeI18N('en')
+      this.$forceUpdate()
+    }
+  }
 }
 </script>
 
--- 09-vue-reusability/src/translatePlugin.js
+++ 09-vue-reusability/src/translatePlugin.js
@@ -1,8 +1,24 @@
 export default {
   // Метод install Должен быть обязательно реализован
   install(app, options) {
+    let current = 'ru'
+
+    const changeLanguage = name => {
+      current = name
+      console.log(current)
+    }
+
     app.config.globalProperties.$alert = text => {
       window.alert(text)
     }
+
+    app.config.globalProperties.$i18n = key => {
+      // key = app.title -> [app, title]
+      return key.split('.').reduce((words, k) => {
+        return words[k] || '=== UNKNOWN ==='
+      }, options[current])
+    }
+
+    app.provide('changeI18N', changeLanguage)
   }
 }
