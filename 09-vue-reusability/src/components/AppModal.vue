5218940aa7520e106faff87dcc5b57be406d3dcf
c0ntr-all <serg.taturin@gmail.com>
2021-12-28 00:34:04 +0300

    09-09: Пример. Модальное окно с Teleport

---
+++ 09-vue-reusability/src/components/AppModal.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="modal-backdrop" @click="$emit('close')"></div>
+  <div class="modal">
+    <h3>Это заголовок модалки</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At expedita obcaecati tempore. Accusantium aliquid beatae deleniti, enim iusto perferendis sit.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ['close']
+}
+</script>
+
+<style scoped>
+
+</style>
