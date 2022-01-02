17d6d76d0d850232c7123c2de964c44f6c94095e
c0ntr-all <serg.taturin@gmail.com>
2022-01-03 00:55:54 +0300

    10-14: Оптимизация с Lazy Loading

--- 10-vue-router/src/router.js
+++ 10-vue-router/src/router.js
@@ -2,9 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Login from './views/Login'
 import Forget from './views/Forget'
 import Dashboard from './views/Dashboard'
-import Mail from './views/Mail'
+// import Mail from './views/Mail'
 import NotFound from './views/NotFound'
 import AppEmailBody from './components/AppEmailBody'
+
+const Mail = () => import('./views/Mail')
 
 const router = createRouter({
   history: createWebHistory(),
