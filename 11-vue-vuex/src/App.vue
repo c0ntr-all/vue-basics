fbd560deec33dbad94469857e6b6304c6dd1256b
c0ntr-all <serg.taturin@gmail.com>
2022-02-12 23:57:46 +0300

    01-02: Начинаем работу со Store

--- 11-vue-vuex/src/App.vue
+++ 11-vue-vuex/src/App.vue
@@ -1,8 +1,10 @@
 <template>
+  <the-navbar></the-navbar>
   <div class="container with-nav">
     <div class="card">
       <h1>Про Vuex</h1>
-      <h2>Счетчик 0</h2>
+      <h2>Счетчик {{ $store.state.counter }}</h2>
+      <button class="btn" @click="increment">Добавить</button>
     </div>
   </div>
 </template>
@@ -10,6 +12,16 @@
 <script>
 import TheNavbar from './TheNavbar'
 export default {
+  // data() {
+  //   return {
+  //     counter: 0
+  //   }
+  // },
+  methods: {
+    increment() {
+      this.$store.state.counter++
+    }
+  },
   components: {TheNavbar}
 }
 </script>
