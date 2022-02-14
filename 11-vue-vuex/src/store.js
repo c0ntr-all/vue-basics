5424068f6c9a28a6d527af5da296c06079f350e7
c0ntr-all <serg.taturin@gmail.com>
2022-02-14 22:55:29 +0300

    11-04: Зачем нужны getters

--- 11-vue-vuex/src/store.js
+++ 11-vue-vuex/src/store.js
@@ -11,8 +11,22 @@ export default createStore({
     increment(state) {
       state.counter++
     },
+    //payload - данные, которые можно передать из компонента
     add(state, payload) {
       state.counter += payload.value
+    }
+  },
+  //Через геттеры можно манипулировать отображением данных не трогая стэйт, например добавить восклицательный знак
+  getters: {
+    counter(state) {
+      // if(state.counter > 50) {
+      //   return state.counter + '!'
+      // }
+      return state.counter
+    },
+    //Первым прааметром принимается state, вторым getters. Если state не используется то можно ставить _.
+    doubleCounter(_, getters) {
+      return getters.counter * 2
     }
   }
 })
