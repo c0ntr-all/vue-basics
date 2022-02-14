01a7f2c2ca1ab004de799340d43ecae5a6eac5a1
c0ntr-all <serg.taturin@gmail.com>
2022-02-14 23:59:30 +0300

    11-07(50%): Разница импортирования мутаций в компонент

--- 11-vue-vuex/src/TheNavbar.vue
+++ 11-vue-vuex/src/TheNavbar.vue
@@ -2,26 +2,35 @@
   <header class="navbar">
     <strong>Счетчик {{ counter }}</strong>
 
-    <button class="btn" @click="add">Добавить 5</button>
+    <button class="btn" @click="addFive">Добавить 5</button>
   </header>
 </template>
 
 <script>
-  export default {
+import {mapGetters, mapMutations} from "vuex";
+
+export default {
     computed: {
-      counter() {
-        return this.$store.getters.counter
-      }
+      // counter() {
+      //   return this.$store.getters.counter
+      // },
+      //Такой подход заменяет конструкцию выше. Мы сразу забираем из vuex все, что касается counter
+      ...mapGetters(['counter'])
     },
     methods: {
-      add() {
+      //Поскольку мутации - это методы, то мы можем через map вывести все мутации как методы здесь
+      //Отсюда доступен метода add из мутаций в кнтексте this
+      ...mapMutations(['add']),
+      addFive() {
+        this.add({value: 5})
+
       //   this.$store.commit('add', {
       //     value: 5
       //   })
-        this.$store.commit({
-          type: 'add',
-          value: 5
-        })
+      //   this.$store.commit({
+      //     type: 'add',
+      //     value: 5
+      //   })
       }
     }
   }
